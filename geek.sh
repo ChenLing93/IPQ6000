7b10a2946a2d2ffa40d7b99a71983f699926cf37
ChenLing93 <99506903+ChenLing93@users.noreply.github.com>
2026-03-01 16:08:33 +0800

    Update geek.sh

--- geek.sh
+++ geek.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # 修改默认IP
-sed -i 's/192.168.100.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.5.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # TTYD 免登录
-# sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
 # 移除要替换的包
 # rm -rf feeds/packages/net/mosdns
@@ -31,21 +31,16 @@ function git_sparse_clone() {
 
 # 添加额外插件
 # git_sparse_clone master https://github.com/sundaqiang/openwrt-packages luci-app-wolplus
-# git_sparse_clone main https://github.com/nikkinikki-org/OpenWrt-nikki nikki
-# git_sparse_clone main https://github.com/nikkinikki-org/OpenWrt-nikki luci-app-nikki
+git_sparse_clone main https://github.com/nikkinikki-org/OpenWrt-nikki nikki
+git_sparse_clone main https://github.com/nikkinikki-org/OpenWrt-nikki luci-app-nikki
 # 添加nikki
-# add feed
-# echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
-# # update & install feeds
-# ./scripts/feeds update -a
-# ./scripts/feeds install -a
+add feed
+echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
+update & install feeds
+./scripts/feeds update -a
+./scripts/feeds install -a
 # # make package
-# make package/luci-app-nikki/compile
-
-
-# 科学上网插件
-
-# Themes
+make package/luci-app-nikki/compile
 
 
 # SmartDNS
@@ -53,12 +48,12 @@ function git_sparse_clone() {
 # git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
 
 # 修改本地时间格式
-# sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
+sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
 
 # 修改版本为编译日期
-# date_version=$(date +"%y.%m.%d")
-# orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-# sed -i "s/${orig_version}/R${date_version} by Haiibo/g" package/lean/default-settings/files/zzz-default-settings
+date_version=$(date +"%y.%m.%d")
+orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+sed -i "s/${orig_version}/R${date_version} by Haiibo/g" package/lean/default-settings/files/zzz-default-settings
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
